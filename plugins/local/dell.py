dbae2c5acead17caf2e901cba53919156f331b51
Ian Munsie <darkstarsword@gmail.com>
2012-03-03 12:39:59 +1100

    Dell local: Add Intuos pad button bindings
    For the moment just set the default bindings to mimic the defaults in
    Windows. I'll no doubt customise this later to suit my workflow better.
    
    I'd like one of the buttons toggle an on-screen keyboard, I think that
    would be pretty useful for editing short text fields rather than
    reaching over the top of the tablet to the keyboard.
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

--- plugins/local/dell.py
+++ plugins/local/dell.py
@@ -17,10 +17,48 @@ keys.bind('main', (
 	))
 
 @notify_exception
+def apply_wacom():
+
+	dev_name = 'Wacom Intuos3 9x12 cursor' # Used to come up as pad, not cursor
+	# (Default mouse button mappings in brackets)
+	#   +-----+-----+ +-------+   \    +-------+ +-----+-----+
+	#   |     |     | |       |    \   |       | |  5  |     |
+	#   |     |  1  | |  ^    |    /   |    ^  | |     |     |
+	#   |     |     | |  |(4) |   /    | (4)|  | |  (9)|     |
+	#   |  3  +-----+ |  |    |   \    |    |  | +-----+  7  |
+	#   |     |     | |       |    \   |       | |  6  |     |
+	#   |     |  2  | |       |    /   |       | |     | (11)|
+	#   |     |     | |  |    |   /    |    |  | | (10)|     |
+	#   +-----+-----+ |  |(5) |   \    | (5)|  | +-----+-----+
+	#   |     4     | |  v    |    \   |    v  | |     8     |
+	#   |        (8)| |       |    /   |       | |       (12)|
+	#   +-----------+ +-------+   /    +-------+ +-----------+
+	mapping = {
+			# Defaults set for Windows:
+			'Button1': 'key shift',
+			'Button2': 'key alt',
+			'Button3': 'key ctrl',
+			'Button4': 'key space',
+
+			'Button5': 'key rshift',
+			'Button6': 'key ralt',
+			'Button7': 'key rctrl',
+			'Button8': 'key space',
+
+			# remapping StripLUp, etc. doesn't seem to be working for me...
+		}
+
+	notify("Applying Wacom Tablet settings")
+	for (prop, action) in mapping.items():
+		_launch(['xsetwacom', 'set', dev_name, prop, action])
+
+@notify_exception
 def fixX11():
 	notify("Applying X11 settings")
 	_launch("setxkbmap -option terminate:ctrl_alt_bksp".split())
 	_launch("setxkbmap -option keypad:pointerkeys".split())
+
+	apply_wacom()
 
 fixX11()
 
