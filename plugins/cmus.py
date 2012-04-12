de51ecd964334f5b24b18133d6f312efbddff9a9
Ian Munsie <darkstarsword@gmail.com>
2012-04-12 14:25:40 +1000

    Add plugin for cmus music player
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

---
+++ plugins/cmus.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+module = sys.modules[__name__]
+
+from pluginmanager import notify_exception
+import music
+
+if __name__ == '__main__':
+	print "Don't call me directly"
+	sys.exit(0)
+
+def unload():
+	music.unregister_music_backend('cmus')
+
+def is_running():
+	try:
+		info = cmus_info()
+		return True
+	except subprocess.CalledProcessError, e:
+		return False
+	assert(False)
+
+def cmus_info():
+	output = subprocess.check_output('cmus-remote -Q'.split()).split('\n')
+	tags = {}
+	status = {}
+	for item in map(str.split, output):
+		if len(item) > 2 and item[0] == 'tag':
+			tags[item[1]] = ' '.join(item[2:])
+		elif len(item) > 1 and item[0] in 'status file duration position'.split():
+			status[item[0]] = ' '.join(item[1:])
+	return (status, tags)
+
+def status():
+	(status, tags) = cmus_info()
+	#print (status, tags)
+	if status['status'] == 'playing':
+		tmp = ''
+		if 'artist' in tags and tags['artist']:
+			tmp += '%s ' % tags['artist']
+			if 'title' in tags and tags['title']:
+				tmp += '- '
+		if 'title' in tags and tags['title']:
+			tmp += '%s ' % tags['title']
+		else:
+			tmp += '%s ' % status['file']
+		return '%s[%s/%s]' % (tmp, status['position'], status['duration'])
+	return None
+
+def cmus_command(*args):
+	subprocess.check_call(['cmus-remote'] + list(args))
+
+commands = {
+	'Play': lambda: cmus_command('-p'),
+	'Play/Pause': lambda: cmus_command('-u'),
+	'Stop': lambda: cmus_command('-s'),
+	'Previous Track': lambda: cmus_command('-r'),
+	'Next Track': lambda: cmus_command('-n'),
+	'Volume Up': lambda: cmus_command('-v', '+%i' % vol_delta),
+	'Volume Down': lambda: cmus_command('-v', '-%i' % vol_delta),
+}
+
+music.register_music_backend('cmus', module)
