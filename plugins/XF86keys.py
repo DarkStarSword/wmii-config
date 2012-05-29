497f49238ebbd6708c80e972a6eeaf21dc6dd52d
Ian Munsie <darkstarsword@gmail.com>
2012-05-29 15:20:53 +1000

    Add shortcuts to release grab on XF86Audio keys
    These can be used to allow other applications to use these keys if
    desired.
    
    I actually did this for spotify, only to discover that it doesn't
    actually grab the keys (ie, it has to be the focussed window). I've
    spotted that spoitfy has a d-bus interface, so I'll just hook that up to
    the music plugin like I have cmus and mocp.
    
    Still, this may be useful for other things, so here it is.
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

--- plugins/XF86keys.py
+++ plugins/XF86keys.py
@@ -3,6 +3,9 @@
 from pluginmanager import notify, notify_exception
 from pygmi import *
 
+audio_keys_grabbed = True
+volume_keys_grabbed = True
+
 if __name__ == '__main__':
 	import sys
 	print "Don't call me directly"
@@ -13,33 +16,82 @@ for mode in keys.modelist:
 		('XF86ScreenSaver',		lambda k: locknow()),
 		('XF86Sleep',			lambda k: locknow()),
 		('XF86Suspend',			lambda k: locknow()),
-		('XF86AudioLowerVolume',	lambda k: intel_vol('down')),
-		('XF86AudioRaiseVolume',	lambda k: intel_vol('up')),
-		('XF86AudioMute',		lambda k: intel_vol('mute')),
 		('%(mod)s-XF86AudioLowerVolume',lambda k: vol('down')),
 		('%(mod)s-XF86AudioRaiseVolume',lambda k: vol('up')),
 		('%(mod)s-XF86AudioMute',	lambda k: vol('mute')),
 		('%(mod)s-XF86AudioPlay',	lambda k: try_launch('mixer')),
 		('%(mod)s-XF86AudioPause',	lambda k: try_launch('mixer')),
-		('XF86AudioPlay',		lambda k: music("Play/Pause")),
-		('XF86AudioPause',		lambda k: music("Play/Pause")),
-		('XF86AudioStop',		lambda k: music("Stop")),
-		('XF86AudioPrev',		lambda k: music("Previous Track")),
-		('XF86AudioNext',		lambda k: music("Next Track")),
+		('%(mod)s-Control-XF86AudioPlay',	lambda k: toggle_audio_grab()),
+		('%(mod)s-Control-XF86AudioPause',	lambda k: toggle_audio_grab()),
+		('%(mod)s-Control-XF86AudioMute',	lambda k: toggle_volume_grab()),
 		('XF86Calculator',		lambda k: try_launch('calculator')),
 		('XF86HomePage',		lambda k: try_launch('webBrowser')),
 		('XF86Mail',			lambda k: try_launch('emailClient')),
 		('XF86Eject',			lambda k: try_launch('eject')),
 		('XF86TouchpadToggle',		lambda k: toggle_trackpad()),
-		),
-		)
+	),)
 
 #try:
 import unload
 keys.cleanup()
 #except:
 	#pass
-keys.mode = keys.mode # Refresh current key bindings - only required when [re]loading plugin after event loop starts
+
+def grab_audio_keys():
+	for mode in keys.modelist:
+		keys.bind(mode, ('Multimedia Keys',
+			('XF86AudioPlay',		lambda k: music("Play/Pause")),
+			('XF86AudioPause',		lambda k: music("Play/Pause")),
+			('XF86AudioStop',		lambda k: music("Stop")),
+			('XF86AudioPrev',		lambda k: music("Previous Track")),
+			('XF86AudioNext',		lambda k: music("Next Track")),
+		),)
+def grab_volume_keys():
+	for mode in keys.modelist:
+		keys.bind(mode, ('Multimedia Keys',
+			('XF86AudioLowerVolume',	lambda k: intel_vol('down')),
+			('XF86AudioRaiseVolume',	lambda k: intel_vol('up')),
+			('XF86AudioMute',		lambda k: intel_vol('mute')),
+		),)
+def release_audio_keys():
+	for mode in keys.modelist:
+		keys.unbind(mode, 'XF86AudioPlay')
+		keys.unbind(mode, 'XF86AudioPause')
+		keys.unbind(mode, 'XF86AudioStop')
+		keys.unbind(mode, 'XF86AudioPrev')
+		keys.unbind(mode, 'XF86AudioNext')
+def release_volume_keys():
+	for mode in keys.modelist:
+		keys.unbind(mode, 'XF86AudioLowerVolume')
+		keys.unbind(mode, 'XF86AudioRaiseVolume')
+		keys.unbind(mode, 'XF86AudioMute')
+
+if audio_keys_grabbed:
+	grab_audio_keys()
+if volume_keys_grabbed:
+	grab_volume_keys()
+keys.mode = keys.mode
+
+def toggle_audio_grab():
+	global audio_keys_grabbed
+	if audio_keys_grabbed:
+		notify("Releasing grab on XF86Audio keys")
+		release_audio_keys()
+	else:
+		notify("Grabbing XF86Audio keys")
+		grab_audio_keys()
+	audio_keys_grabbed = not audio_keys_grabbed
+	keys.mode = keys.mode
+def toggle_volume_grab():
+	global volume_keys_grabbed
+	if volume_keys_grabbed:
+		notify("Releasing grab on XF86Audio volume keys")
+		release_volume_keys()
+	else:
+		notify("Grabbing XF86Audio volume keys")
+		grab_volume_keys()
+	volume_keys_grabbed = not volume_keys_grabbed
+	keys.mode = keys.mode
 
 @notify_exception
 def toggle_trackpad():
