2b13fb28e745318b5fb7da0a97d1f5d16262c2e8
Ian Munsie <darkstarsword@gmail.com>
2012-08-01 12:18:40 +1000

    spotify: Blacklist another ad
    I'm guessing that McDonald's will have multiple ads, in case of false
    positives the specific title was "Social Playlist By Macca's"
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -19,6 +19,7 @@ ad_blacklist = [
 		#
 		# TODO: Add other known ads as I come across them
 		('Spotify', 'Spotify'),
+		"McDonald's",
 		('Indies ANZ (InHouse)', 'Tiesto'),
 		('EMI Music Australia (InHouse)', 'Blur'),
 		# Hmmm... What's the deal with the (InHouse) prefix here? I
