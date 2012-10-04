ce490af580901e436b8cafc80f30a7e85be283e2
Ian Munsie <darkstarsword@gmail.com>
2012-10-04 22:57:30 +1000

    EeePC: Add auto suspend toggle key
    This depends on my custom ACPI suspend scripts, see:
    https://sites.google.com/site/darkstarjunkspace/eee-pc/acpi-scripts
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

--- plugins/local/eeepc.py
+++ plugins/local/eeepc.py
@@ -1,6 +1,27 @@
-from pluginmanager import imported_from_wmiirc
+from pygmi import *
+
+from pluginmanager import notify, notify_exception, imported_from_wmiirc
 
 if imported_from_wmiirc():
   import wmiirc
   wmiirc.wmii['font'] = '6x9'
   wmiirc.wmii['fontpad'] = '0 0 -1 -1'
+
+keys.bind('main', (
+	"EeePC specific keys",
+	('%(mod)s-F1', "Toggle Auto Suspend",
+		lambda k: toggle_auto_suspend()),
+	))
+
+@notify_exception
+def toggle_auto_suspend():
+	import os
+	file = '/tmp/noautosuspend'
+	if os.path.exists(file):
+		os.remove(file)
+		notify('Auto Suspend Enabled', 'suspend')
+	else:
+		open(file, 'w').close()
+		notify('Auto Suspend Disabled', 'suspend')
+
+keys.mode = keys.mode # Refresh current key bindings - only required when [re]loading plugin after event loop starts
